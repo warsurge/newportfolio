fea217d58a640d8e46268b65c192a6f99a5a9994
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-05-31 12:53:41 -0500

    Updates

---
+++ precache-manifest.2a37767bb117f883fa6a4eb4b1fd6e28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8c95c1370b411438efd1ee9be980cd",
+    "url": "/newportfolio/index.html"
+  },
+  {
+    "revision": "4473da16910006cd11fa",
+    "url": "/newportfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4813c4d193b2e574877e",
+    "url": "/newportfolio/static/js/2.20dfc9b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newportfolio/static/js/2.20dfc9b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4473da16910006cd11fa",
+    "url": "/newportfolio/static/js/main.8bff4db1.chunk.js"
+  },
+  {
+    "revision": "604cbbf478abe9526335",
+    "url": "/newportfolio/static/js/runtime-main.2db5c7e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newportfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
