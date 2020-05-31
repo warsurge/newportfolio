f86f0fac17fcea0d0176c0c97afd5f11bc82c13b
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-05-31 13:05:26 -0500

    added resume, added selfie, changed app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import Home from "../src/pages/home"
+import Contact from "../src/pages/contact"
+import Portfolio from "../src/pages/portfolio"
+import "bootswatch/dist/lux/bootstrap.min.css";
+import Nav from "../src/components/Nav"
+import Footer from "../src/components/Footer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Nav />
+
+      <Switch>
+        <Route exact path={["/reactportfolio", "reactportfolio/home"]}>
+          <Home />
+        </Route>
+        <Route exact path={["/reactportfolio/portfolio"]}>
+          <Portfolio />
+        </Route>
+        <Route exact path={["/reactportfolio/contact"]}>
+          <Contact />
+        </Route>
+
+      </Switch>
+      <Footer />
+
+    </BrowserRouter>
   );
 }
 
